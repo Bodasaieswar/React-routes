6ea850ac5eb819a9a2b50b39991911c213b8d0b4
sboda <sboda@arizona.edu>
2023-06-07 11:34:41 -0700

    star functionality with optimization

--- react-routes/src/routes/contact.jsx
+++ react-routes/src/routes/contact.jsx
@@ -88,6 +88,9 @@ function Favorite({ contact }) {
 	// yes, this is a `let` for later
 	const fetcher = useFetcher();
 	let favorite = contact.favorite;
+	if (fetcher.formData) {
+		favorite = fetcher.formData.get('favorite') === 'true';
+	}
 
 	return (
 		<fetcher.Form method="post">
